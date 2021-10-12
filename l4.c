1e677d03d906e908f9995140a8159548476f7a6a
HuYiyang124 <38487000+HuYiyang124@users.noreply.github.com>
2021-10-12 14:01:47 +0800

    Create l4.c

---
+++ l4.c
@@ -0,0 +1,23 @@
+
+#include <cstdio>
+#include <stdlib.h>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int x,tmp;
+    printf("Enter a dollar amount:");
+    scanf("%d",&x);
+    tmp=x;
+    printf("\n$20 bills: %d\n",tmp/20);
+    tmp=tmp%20;
+    printf("$10 bills: %d\n",tmp/10);
+    tmp=tmp%10;
+    printf("$5 bills: %d\n",tmp/5);
+    tmp=tmp%5;
+    printf("$1 bills: %d\n",tmp);
+    system("PAUSE");
+    return 0;
+}
