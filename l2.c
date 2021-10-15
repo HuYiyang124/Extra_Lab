1bc4975bdc8c873b6eff5f1af1d14a9014641a4a
HuYiyang124 <38487000+HuYiyang124@users.noreply.github.com>
2021-10-15 14:24:27 +0800

    Delete l2.c

--- l2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int r = 10; // define the radius of the sphere
-	float v;
-	v = 4/3. * 3.14 * r * r * r; // calculate the volume
-	printf("The volume of the sphere is : %.2f\n", v);
-	return 0;
-}
