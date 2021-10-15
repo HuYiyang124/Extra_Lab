53f23d1d5c1fa853b3d87b12faaaa43c9281bde7
HuYiyang124 <38487000+HuYiyang124@users.noreply.github.com>
2021-10-15 14:27:56 +0800

    Add files via upload

---
+++ Assignment1/t4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char rawChar1, rawChar2, pChar1, pChar2;
+    printf("Input: ");
+    scanf("%c %c", &rawChar1, &rawChar2);
+
+    pChar1 = (rawChar1<='z' && rawChar1>='a')?(rawChar1 - ('a' - 'A')):rawChar1;
+    pChar2 = (rawChar2<='z' && rawChar2>='a')?(rawChar2 - ('a' - 'A')):rawChar2;
+
+    printf("Output: ");
+
+    if(pChar1<='Z' && pChar1>='A' && pChar2<='Z' && pChar2>='A')
+        printf("%c %c\n", pChar1>pChar2?rawChar2:rawChar1, pChar1<=pChar2?rawChar2:rawChar1);
+    else if(pChar1>'Z' || pChar1<'A')
+    {
+        if(pChar2>'Z' || pChar2<'A')
+            printf("%c and %c both are not an English letter.\n", rawChar1, rawChar2);
+        else
+            printf("%c is not an English letter.\n", rawChar1);
+    }
+    else
+        printf("%c is not an English letter.\n", rawChar2);
+
+    return 0;
+}
