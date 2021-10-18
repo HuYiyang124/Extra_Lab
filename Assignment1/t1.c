a744d2754b5a75f584b99fc64ffe1ea13ff6c5c7
HuYiyang124 <38487000+HuYiyang124@users.noreply.github.com>
2021-10-18 11:11:15 +0800

    Update t1.c

--- Assignment1/t1.c
+++ Assignment1/t1.c
@@ -15,15 +15,12 @@ int main(){
     return 0;
 }
 /*
- (1) The first error is the scanf less a &
- The second error is in the forth printf, 
- cann't use %d to print a float variable
- The third error is the last two printf less the ;
+ (1) The last two printf less the ;
 */
 /*
- (2) It has two warnning 
- The first warnning is the main function have not a type
- it need add a int and add a return value
- The second warnning is in the second printf 
- i is not initilized, but the function directly output it
+ (2) It has forth warnning 
+ The first warnning is the main function have not a type, it need add a int and add a return value
+ The second warnning is in the second printf, i is not initilized, but the function directly output it
+ The third warnning is the scanf less a &
+ The forth warnning is in the forth printf, cann't use %d to print a float variable
 */
